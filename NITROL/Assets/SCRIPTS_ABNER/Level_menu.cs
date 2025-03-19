a5d187f78f086ccd92c2a543fecc4d5aa72035be
Abner Alain  Gil Garcia <abnergarcia195@gmail.com>
2025-03-18 18:00:39 -0600

    Se sube menu del juego y de niveles

---
+++ NITROL/Assets/SCRIPTS_ABNER/Level_menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level_menu : MonoBehaviour {
+
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for(int i=0; i<buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i=0; i<unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+    public void OpenLevel(int levelID)
+    {
+        string levelName = "ESCENA_LVL" + levelID;
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Playlvl1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+}
