cb1d48727f4788a2a1f89c8f765352191a2a01fe
Abner Alain  Gil Garcia <abnergarcia195@gmail.com>
2025-03-18 18:36:45 -0600

    Se sube bandera

---
+++ NITROL/Assets/Finish_Point.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Finish_Point : MonoBehaviour {
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (colission.CompareTag("Player"))
+        {
+            SceneController.instance.NextLevel();
+        }
+    }*/
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+}
